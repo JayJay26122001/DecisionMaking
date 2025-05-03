4dd4d54869c8ac5f57a14758c31f67fefa1dbc0f
JayJay26122001 <matheus.jacoby.hahn@gmail.com>
2025-05-03 15:52:25 -0300

    Métodos

--- DecisionMaking/Assets/Scripts/Chase.cs
+++ DecisionMaking/Assets/Scripts/Chase.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class Chase : Action
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override void ActiveAction()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
